4bac7becce647cbba01f660553ab2db6009a28c2
xtxtxt2568 <14284183+xtxtxt2568@user.noreply.gitee.com>
2026-05-31 23:34:26 +0800

    fix: simplify blog navigation

--- site.config.mjs
+++ site.config.mjs
@@ -1,6 +1,6 @@
 export const siteConfig = {
   siteTitle: "2568xt",
-  siteDescription: "关于视觉、模型与代码的简洁笔记。",
+  siteDescription: "2568xt",
   siteUrl: "https://2568xt.github.io",
   basePath: "/blog",
   author: "2568xt",
